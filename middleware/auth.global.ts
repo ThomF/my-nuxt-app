576e2131fd173fb636681b72e126126c5a3a818e
Thomas Fuhriman <seniorswanky@icloud.com>
2025-02-26 16:47:37 -0700

    comment of thoughts

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,6 +2,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const userCookie = useCookie('auth_user'); // Read stored auth state
 
   // 🚨 Prevent infinite loop: If already on /login, don't redirect
+  // fairly certain this doesnt do anything since i am sure we are not storing any cookies
   if (!userCookie.value && to.path !== '/login') {
     return navigateTo('/login'); 
   }
